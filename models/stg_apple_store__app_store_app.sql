ea38fba11919d00b0c8025a804f3b5bf14d00d39
Renee Li <renee.li@fivetran.com>
2025-01-15 23:08:16 -0500

    app table has changed to app_store_app

--- models/stg_apple_store__app_store_app.sql
+++ models/stg_apple_store__app_store_app.sql
@@ -1,7 +1,7 @@
 with base as (
 
     select * 
-    from {{ ref('stg_apple_store__app_tmp') }}
+    from {{ ref('stg_apple_store__app_store_app_tmp') }}
 ),
 
 fields as (
@@ -9,8 +9,8 @@ fields as (
     select
         {{
             fivetran_utils.fill_staging_columns(
-                source_columns=adapter.get_columns_in_relation(ref('stg_apple_store__app_tmp')),
-                staging_columns=get_app_columns()
+                source_columns=adapter.get_columns_in_relation(ref('stg_apple_store__app_store_app_tmp')),
+                staging_columns=get_app_store_app_columns()
             )
         }}
         
@@ -28,8 +28,8 @@ final as (
     select
         cast(source_relation as {{ dbt.type_string() }}) as source_relation, 
         cast(id as {{ dbt.type_bigint() }}) as app_id,
-        cast(name as {{ dbt.type_string() }}) as app_name,
-        is_enabled
+        cast(name as {{ dbt.type_string() }}) as app_name
+
     from fields
 )
 
