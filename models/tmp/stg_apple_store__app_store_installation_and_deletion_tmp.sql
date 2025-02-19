713789c72b1623417eaea2908ddc262d4200d663
Renee Li <renee.li@fivetran.com>
2025-02-19 14:27:43 -0500

    try without default schema change

--- models/tmp/stg_apple_store__app_store_installation_and_deletion_tmp.sql
+++ models/tmp/stg_apple_store__app_store_installation_and_deletion_tmp.sql
@@ -4,7 +4,7 @@
         database_variable='apple_store_database', 
         schema_variable='apple_store_schema', 
         default_database=target.database,
-        default_schema='itunes_connect',
+        default_schema='apple_store',
         default_variable='app_store_installation_and_deletion_standard_daily',
         union_schema_variable='apple_store_union_schemas',
         union_database_variable='apple_store_union_databases'
--- models/tmp/stg_apple_store__sales_subscription_summary_tmp.sql
+++ models/tmp/stg_apple_store__sales_subscription_summary_tmp.sql
@@ -5,7 +5,7 @@
         database_variable='apple_store_database', 
         schema_variable='apple_store_schema', 
         default_database=target.database,
-        default_schema='itunes_connect',
+        default_schema='apple_store',
         default_variable='sales_subscription_summary',
         union_schema_variable='apple_store_union_schemas',
         union_database_variable='apple_store_union_databases'
